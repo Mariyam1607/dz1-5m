bc6e98123c79d84e9498c478892ec7b4667d68fc
Mariyam <attoeva01@bk.ru>
2026-04-01 16:28:12 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// // import './App.css'
+// import {createRoot} from "react-dom/client";
+
+import Element from "./components/Element.jsx";
+
+function App() {
+
+  return (
+      <div>
+          <h2>App.jsx</h2>
+          <h1>Hello, World!</h1>
+          <p>Сейчас я только начинаю свой путь в программировании,
+              и каждый новый проект в обучении - это маленький шаг,
+              но в переди еще много всего интересного.</p>
+          <Element />
+      </div>
+  );
+}
+
+export default App
---
+++ src/components/Element1.jsx
@@ -0,0 +1,5 @@
+const Element1 = () => {
+    return <div>Element1</div>
+};
+
+export default Element1;
